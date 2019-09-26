d5ae1ecf4e7196918259f31227744daee0209ca6
lukairui <774729207@qq.com>
2019-09-26 13:42:49 +0800

    first commit on vuejs project

---
+++ src/components/G2Line.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="mountNode"></div>
+</template>
+
+<script>
+import G6 from '@antv/g6'
+let data = {
+  nodes: [{
+    id: 'node1',
+    x: 100,
+    y: 200
+  },
+  {
+    id: 'node2',
+    x: 300,
+    y: 200
+  }],
+  edges: [{
+    source: 'node1',
+    target: 'node2'
+  }]
+}
+let graph = new G6.Graph({
+  container: 'mountNode',
+  width: 500,
+  height: 500
+})
+graph.data(data)
+graph.render()
+export default {
+  data () {
+    return {
+      chart: 'test'
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="mountNode"></div>
+</template>
+
+<script>
+import G6 from '@antv/g6'
+
+export default {
+  data () {
+    return {
+      chart: 'test'
+    }
+  },
+  mounted () {
+    let graph = new G6.Graph({
+      container: 'mountNode',
+      width: 500,
+      height: 500
+    })
+    let data = {
+      nodes: [{
+        x: 100,
+        y: 100,
+        shape: 'circle',
+        label: 'circle'
+      },
+      {
+        x: 200,
+        y: 100,
+        shape: 'rect',
+        label: 'rect'
+      },
+      {
+        x: 300,
+        y: 100,
+        size: [60, 30],
+        shape: 'ellipse',
+        label: 'ellipse'
+      },
+      {
+        x: 400,
+        y: 100,
+        shape: 'image',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg',
+        label: 'image'
+      }]
+    }
+    console.log('created')
+    graph.data(data)
+    graph.render()
+  }
+}
+</script>
